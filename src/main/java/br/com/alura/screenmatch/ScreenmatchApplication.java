93fa0b498be99a51d81c508c1af5d14f543edec7
Marco Tortelli <marcopazinitortelli@gmail.com>
2024-08-09 13:03:07 -0300

    Subindo o projeto no GitHub

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,33 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
+
+
+//		List<DadosTemporada> temporadas = new ArrayList<>();
+//
+//		for (int i = 1; i <= dados.totalTemporadas(); i++){
+//			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=e692fb07");
+//			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+//			temporadas.add(dadosTemporada);
+//		}
+//
+//		temporadas.forEach(System.out::println);
+
+	}
+}
